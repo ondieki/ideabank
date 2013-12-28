1bcda347d69d4cf43f1f0472c5d831bf099684b1
Bavin Ondieki <bavin2009@gmail.com>
2013-12-28 11:53:44 -0800

    initial commit for ideabank

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ideabank::Application.config.secret_key_base = '5a97beb7fbb0480c0d146187298fdf190d8ec33e573976f20427b52a128bc28bb953effc2863ef5d1bce16351665162c0fda52731dde318678a016f629510c82'
