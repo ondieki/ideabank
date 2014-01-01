173ff69b1429260949f7a62ae1a917b079b515d3
Bavin Ondieki <bavin2009@gmail.com>
2014-01-01 09:27:43 -0800

    changed environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,11 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Ideabank::Application.initialize!
 
-ActionMailer::Base.delivery_method = :sendmail
-ActionMailer::Base.perform_deliveries = true
-ActionMailer::Base.raise_delivery_errors = true
-ActionMailer::Base.default_charset = "utf-8"
+  ActionMailer::Base.delivery_method = :sendmail
+  ActionMailer::Base.sendmail_settings = {
+        :location       => '/usr/sbin/sendmail',
+        :arguments      => '-i -t'
+  }
+
+  ActionMailer::Base.perform_deliveries = true # the "deliver_*" methods are available
+  ActionMailer::Base.raise_delivery_errors = true
