3b3ebd32a47a1d7464e4e3f01cd26a9e175dfd9d
Bavin Ondieki <bavin2009@gmail.com>
2014-01-01 02:22:20 -0800

    changed environment variables for actionmailer

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,10 @@
+class UserMailer < ActionMailer::Base
+  default from: "bavin2009@gmail.com"
+
+
+  def welcome_email(user)
+  	@user = user
+	@url = 'localhost:3000/user/login'
+	mail(to: @user.email, subject: 'Welcome to Idea Bank')
+  end
+end
