15925d1f9dd0bfa5c847cb49f3c81be4a0dd83ea
Bavin Ondieki <bavin2009@gmail.com>
2014-01-01 02:21:07 -0800

    changed environment variables for actionmailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,16 @@ Ideabank::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "example.com",
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["GMAIL_USERNAME"],
+  password: ENV["GMAIL_PASSWORD"]
+  
+}
+
 end
