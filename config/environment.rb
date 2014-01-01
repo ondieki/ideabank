39087e5e443661d91fd6d6ac33b8fa0db9c58777
Bavin Ondieki <bavin2009@gmail.com>
2014-01-01 10:36:49 -0800

    ..edited environment.rb, having checked out bug on Stack overflow

--- config/environment.rb
+++ config/environment.rb
@@ -1,14 +1,28 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+Ideabank::Application.configure do
+	config.action_mailer.delivery_method = :smtp
+	config.action_mailer.raise_delivery_errors = true
+	config.action_mailer.smtp_settings = {
+	  :address              => "smtp.gmail.com",
+	  :port                 => 587,
+	  :domain               => 'gmail.com',
+	  :user_name            => ENV["GMAIL_USERNAME"],
+	  :password             => ENV["GMAIL_PASSWORD"],
+	  :authentication       => :plain,
+	  :enable_starttls_auto => true  }
+end
+
 # Initialize the Rails application.
-Ideabank::Application.initialize!
+ Ideabank::Application.initialize!
 
-  ActionMailer::Base.delivery_method = :sendmail
-  ActionMailer::Base.sendmail_settings = {
-        :location       => '/usr/sbin/sendmail',
-        :arguments      => '-i -t'
-  }
+	
+  #ActionMailer::Base.delivery_method = :sendmail
+  #ActionMailer::Base.sendmail_settings = {
+   #     :location       => '/usr/sbin/sendmail',
+   #     :arguments      => '-i -t'
+  #}
 
-  ActionMailer::Base.perform_deliveries = true # the "deliver_*" methods are available
-  ActionMailer::Base.raise_delivery_errors = true
+  #ActionMailer::Base.perform_deliveries = true # the "deliver_*" methods are available
+  #ActionMailer::Base.raise_delivery_errors = true
